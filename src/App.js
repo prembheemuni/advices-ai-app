5cf7b33edc3e2be9348c16beaf69dba718a7c042
durga bhavani <prem8309090125@gmail.com>
2022-06-29 16:59:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Axios from "axios";
+import { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey =
+  "6602e6b4e3092fff5af22cbb2fa30bff2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
+  const [advise, setAdvise] = useState("");
+  const fetchCall = async () => {
+    const response = await Axios.get("https://api.adviceslip.com/advice");
+    const data = await response.data;
+    const msg = data?.slip.advice
+    setAdvise(msg);
+    return msg;
+  };
+
+  useEffect(() => {
+    fetchCall();
+    const alanInstance = alanBtn({
+      key: alanKey,
+      onCommand: async({ command }) => {
+        if (command === "speak") {
+          const ab= await fetchCall();
+          alanInstance.playText(ab);
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homebg">
+        <div className="instructions">
+            
+        </div>
+      <div className="container">
+      <h3>Ask "Say Something"</h3>
+        <h1>{advise}</h1>
+        <button onClick={fetchCall}>Give me Advise</button>
+      </div>
     </div>
   );
 }
---
+++ src/anime.js
@@ -0,0 +1,9 @@
+import { motion } from "framer-motion";
+
+import React from "react";
+
+function anime() {
+  return <motion.div>Prem</motion.div>;
+}
+
+export default anime;
